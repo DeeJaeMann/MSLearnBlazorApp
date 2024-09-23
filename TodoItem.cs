d465e7393c15929a2e285034eb67dcb1ae3a429e
Dee Jae Mann <dee.j.mann@hotmail.com>
2024-09-23 11:40:48 -0400

    Added TodoItem
    Added class
    Added render method for list in component

---
+++ TodoItem.cs
@@ -0,0 +1,11 @@
+﻿namespace MSLearnBlazorApp
+{
+    /// <summary>
+    /// Item for task tracked in Todo component
+    /// </summary>
+    public class TodoItem
+    {
+        public string? Title { get; set; }
+        public bool IsDone { get; set; } = false;
+    }
+}
